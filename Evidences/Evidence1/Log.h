6de90cdfa19cd0584e3b740a76312246cf3f2da0
Regina Mugica Limon <a00843542@tec.mx>
2026-09-18 16:55:21 -0600

    LOG TERMINADO

--- Evidences/Evidence1/Log.h
+++ Evidences/Evidence1/Log.h
@@ -1,7 +1,10 @@
-#ifndef Log_h// instruccion para el compilador
-#include<vector>
+#ifndef Log_h
+#define Log_h
+
+#include <vector>
 #include <iostream>
-#include<string>
+#include <string>
+
 using namespace std;
 
 struct Log{
@@ -12,21 +15,126 @@ struct Log{
     string ip;
     string message;
     string key;
-    //constructores
+
+    // constructores
     Log();
-    Log(int year; string month, int day, string time, string ip, string message, string key);
-    //metodos
+
+    Log(int year, string month, int day, string time,
+        string ip, string message, string key);
+
+    // metodos
     string createKey();
-    bool operator>(const Log&other)const;
-    bool operator<(const Log&other)const;
-    bool operator>=(const Log&other)const;
-    bool operator<=(const Log&other)const;
-    bool operator==(const Log&other)const;
+
+    bool operator>(const Log& other) const;
+    bool operator<(const Log& other) const;
+    bool operator>=(const Log& other) const;
+    bool operator<=(const Log& other) const;
+    bool operator==(const Log& other) const;
 };
 
-bool Log:: operator>(const Log&lother,const{
-    return key>other.key;
+
+// constructor vacio
+Log::Log(){
+    year = 0;
+    month = "";
+    day = 0;
+    time = "";
+    ip = "";
+    message = "";
+    key = "";
 }
 
 
-#define Log_h
+// constructor con datos
+Log::Log(int year, string month, int day, string time,
+         string ip, string message, string key){
+
+    this->year = year;
+    this->month = month;
+    this->day = day;
+    this->time = time;
+    this->ip = ip;
+    this->message = message;
+    this->key = key;
+}
+
+
+// crear llave para ordenar por fecha y hora
+string Log::createKey(){
+
+    string numMonth;
+
+    if(month == "Jan"){
+        numMonth = "01";
+    }
+    else if(month == "Feb"){
+        numMonth = "02";
+    }
+    else if(month == "Mar"){
+        numMonth = "03";
+    }
+    else if(month == "Apr"){
+        numMonth = "04";
+    }
+    else if(month == "May"){
+        numMonth = "05";
+    }
+    else if(month == "Jun"){
+        numMonth = "06";
+    }
+    else if(month == "Jul"){
+        numMonth = "07";
+    }
+    else if(month == "Aug"){
+        numMonth = "08";
+    }
+    else if(month == "Sep"){
+        numMonth = "09";
+    }
+    else if(month == "Oct"){
+        numMonth = "10";
+    }
+    else if(month == "Nov"){
+        numMonth = "11";
+    }
+    else if(month == "Dec"){
+        numMonth = "12";
+    }
+
+    string numDay;
+
+    if(day < 10){
+        numDay = "0" + to_string(day);
+    }
+    else{
+        numDay = to_string(day);
+    }
+
+    key = to_string(year) + numMonth + numDay + time;
+
+    return key;
+}
+
+
+// operadores
+bool Log::operator>(const Log& other) const{
+    return key > other.key;
+}
+
+bool Log::operator<(const Log& other) const{
+    return key < other.key;
+}
+
+bool Log::operator>=(const Log& other) const{
+    return key >= other.key;
+}
+
+bool Log::operator<=(const Log& other) const{
+    return key <= other.key;
+}
+
+bool Log::operator==(const Log& other) const{
+    return key == other.key;
+}
+
+#endif
